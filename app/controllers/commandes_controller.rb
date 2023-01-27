d2da5a6923e0f073fecf8d386bc638c22322e60e
Olivier Barthelemy <olivier.barthelemy1993@gmail.com>
2023-01-27 10:00:40 +0100

    test-solve

--- app/controllers/commandes_controller.rb
+++ app/controllers/commandes_controller.rb
@@ -1,8 +1,8 @@
 class CommandesController < ApplicationController
   def create
-    # if params[:event_team_id].nil?
+    if params[:event_team_id].nil?
       article = Article.find(params[:article_id])
-      order = Commande.create!( article: article, amount_cents: article.price, etat: 'en attente', user_id: current_user.id)
+      order = Commande.create!(article: article, amount_cents: article.price, etat: 'en attente', user_id: current_user.id)
       session = Stripe::Checkout::Session.create(
         payment_method_types: ['card'],
         line_items: [{
@@ -23,31 +23,32 @@ class CommandesController < ApplicationController
       )
       order.update(checkout_session_id: session.id)
       redirect_to new_commande_payment_path(order.id)
-    # else
-    #   team = EventTeam.find(params[:event_team_id])
-    #   article = Article.where(event_id: team.event.id).first
-    #   order = Commande.create!(article: article, event_team: team, amount_cents: team.event.price, etat: 'en attente', user_id: current_user.id)
-    #   session = Stripe::Checkout::Session.create(
-    #     payment_method_types: ['card'],
-    #     line_items: [{
-    #       price_data: {
-    #         unit_amount: team.event.price_cents,
-    #         currency: 'eur',
-    #         product_data: {
-    #           name: team.nom_equipe,
-    #           description: team.niveau
-    #           # images: [team.photo_url],
-    #         }
-    #       },
-    #       quantity: 1
-    #     }],
-    #     mode: 'payment',
-    #     success_url: commandes_url(order),
-    #     cancel_url: commandes_url(order)
-    #   )
-    #   order.update(checkout_session_id: session.id)
-    #   redirect_to new_commande_payment_path(order.id)
-    # end
+
+    else
+      team = EventTeam.find(params[:event_team_id])
+      article = Article.where(event_id: team.event.id).first
+      order = Commande.create!(article: article, event_team: team, amount_cents: team.event.price, etat: 'en attente', user_id: current_user.id)
+      session = Stripe::Checkout::Session.create(
+        payment_method_types: ['card'],
+        line_items: [{
+          price_data: {
+            unit_amount: team.event.price_cents,
+            currency: 'eur',
+            product_data: {
+              name: team.nom_equipe,
+              description: team.niveau
+              # images: [team.photo_url],
+            }
+          },
+          quantity: 1
+        }],
+        mode: 'payment',
+        success_url: commandes_url(order),
+        cancel_url: commandes_url(order)
+      )
+      order.update(checkout_session_id: session.id)
+      redirect_to new_commande_payment_path(order.id)
+    end
   end
 
   def index
