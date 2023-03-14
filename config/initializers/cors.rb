75f1a040bd949781995be622234e151de5b09bf1
Olivier Barthelemy <olivier.barthelemy1993@gmail.com>
2023-03-14 16:19:48 +0100

    prog_cors_2

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://esvl.herokuapp.com/articles'
+    origins 'https://esvl.herokuapp.com/'
     resource '/commandes', headers: :any, methods: [:get, :post]
   end
 end
